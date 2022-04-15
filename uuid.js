acf27166acbc79d65aeca1675bf2d5083f649526
LongYinan <lynweklm@gmail.com>
2022-04-15 23:09:17 +0800

    chore: upgrade dependencies

--- uuid.js
+++ uuid.js
@@ -1,215 +1,241 @@
-const { existsSync, readFileSync } = require("fs");
-const { join } = require("path");
+const { existsSync, readFileSync } = require('fs')
+const { join } = require('path')
 
-const { platform, arch } = process;
+const { platform, arch } = process
 
-let nativeBinding = null;
-let localFileExisted = false;
-let isMusl = false;
-let loadError = null;
+let nativeBinding = null
+let localFileExisted = false
+let loadError = null
+
+function isMusl() {
+  // For Node 10
+  if (!process.report || typeof process.report.getReport !== 'function') {
+    try {
+      return readFileSync('/usr/bin/ldd', 'utf8').includes('musl')
+    } catch (e) {
+      return true
+    }
+  } else {
+    const { glibcVersionRuntime } = process.report.getReport().header
+    return !glibcVersionRuntime
+  }
+}
 
 switch (platform) {
-  case "android":
-    if (arch !== "arm64") {
-      throw new Error(`Unsupported architecture on Android ${arch}`);
+  case 'android':
+    switch (arch) {
+      case 'arm64':
+        localFileExisted = existsSync(join(__dirname, 'uuid.android-arm64.node'))
+        try {
+          if (localFileExisted) {
+            nativeBinding = require('./uuid.android-arm64.node')
+          } else {
+            nativeBinding = require('@napi-rs/uuid-android-arm64')
+          }
+        } catch (e) {
+          loadError = e
+        }
+        break
+      case 'arm':
+        localFileExisted = existsSync(join(__dirname, 'uuid.android-arm-eabi.node'))
+        try {
+          if (localFileExisted) {
+            nativeBinding = require('./uuid.android-arm-eabi.node')
+          } else {
+            nativeBinding = require('@napi-rs/uuid-android-arm-eabi')
+          }
+        } catch (e) {
+          loadError = e
+        }
+        break
+      default:
+        throw new Error(`Unsupported architecture on Android ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, "uuid.android-arm64.node"));
+    break
+  case 'win32':
+    switch (arch) {
+      case 'x64':
+        localFileExisted = existsSync(
+          join(__dirname, 'uuid.win32-x64-msvc.node')
+        )
+        try {
+          if (localFileExisted) {
+            nativeBinding = require('./uuid.win32-x64-msvc.node')
+          } else {
+            nativeBinding = require('@napi-rs/uuid-win32-x64-msvc')
+          }
+        } catch (e) {
+          loadError = e
+        }
+        break
+      case 'ia32':
+        localFileExisted = existsSync(
+          join(__dirname, 'uuid.win32-ia32-msvc.node')
+        )
+        try {
+          if (localFileExisted) {
+            nativeBinding = require('./uuid.win32-ia32-msvc.node')
+          } else {
+            nativeBinding = require('@napi-rs/uuid-win32-ia32-msvc')
+          }
+        } catch (e) {
+          loadError = e
+        }
+        break
+      case 'arm64':
+        localFileExisted = existsSync(
+          join(__dirname, 'uuid.win32-arm64-msvc.node')
+        )
+        try {
+          if (localFileExisted) {
+            nativeBinding = require('./uuid.win32-arm64-msvc.node')
+          } else {
+            nativeBinding = require('@napi-rs/uuid-win32-arm64-msvc')
+          }
+        } catch (e) {
+          loadError = e
+        }
+        break
+      default:
+        throw new Error(`Unsupported architecture on Windows: ${arch}`)
+    }
+    break
+  case 'darwin':
+    switch (arch) {
+      case 'x64':
+        localFileExisted = existsSync(join(__dirname, 'uuid.darwin-x64.node'))
+        try {
+          if (localFileExisted) {
+            nativeBinding = require('./uuid.darwin-x64.node')
+          } else {
+            nativeBinding = require('@napi-rs/uuid-darwin-x64')
+          }
+        } catch (e) {
+          loadError = e
+        }
+        break
+      case 'arm64':
+        localFileExisted = existsSync(
+          join(__dirname, 'uuid.darwin-arm64.node')
+        )
+        try {
+          if (localFileExisted) {
+            nativeBinding = require('./uuid.darwin-arm64.node')
+          } else {
+            nativeBinding = require('@napi-rs/uuid-darwin-arm64')
+          }
+        } catch (e) {
+          loadError = e
+        }
+        break
+      default:
+        throw new Error(`Unsupported architecture on macOS: ${arch}`)
+    }
+    break
+  case 'freebsd':
+    if (arch !== 'x64') {
+      throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
+    }
+    localFileExisted = existsSync(join(__dirname, 'uuid.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require("./uuid.android-arm64.node");
+        nativeBinding = require('./uuid.freebsd-x64.node')
       } else {
-        nativeBinding = require("@napi-rs/uuid-android-arm64");
+        nativeBinding = require('@napi-rs/uuid-freebsd-x64')
       }
     } catch (e) {
-      loadError = e;
+      loadError = e
     }
-    break;
-  case "win32":
+    break
+  case 'linux':
     switch (arch) {
-      case "x64":
-        localFileExisted = existsSync(
-          join(__dirname, "uuid.win32-x64-msvc.node")
-        );
-        try {
-          if (localFileExisted) {
-            nativeBinding = require("./uuid.win32-x64-msvc.node");
-          } else {
-            nativeBinding = require("@napi-rs/uuid-win32-x64-msvc");
-          }
-        } catch (e) {
-          loadError = e;
-        }
-        break;
-      case "ia32":
-        localFileExisted = existsSync(
-          join(__dirname, "uuid.win32-ia32-msvc.node")
-        );
-        try {
-          if (localFileExisted) {
-            nativeBinding = require("./uuid.win32-ia32-msvc.node");
-          } else {
-            nativeBinding = require("@napi-rs/uuid-win32-ia32-msvc");
-          }
-        } catch (e) {
-          loadError = e;
-        }
-        break;
-      case "arm64":
-        localFileExisted = existsSync(
-          join(__dirname, "uuid.win32-arm64-msvc.node")
-        );
-        try {
-          if (localFileExisted) {
-            nativeBinding = require("./uuid.win32-arm64-msvc.node");
-          } else {
-            nativeBinding = require("@napi-rs/uuid-win32-arm64-msvc");
-          }
-        } catch (e) {
-          loadError = e;
-        }
-        break;
-      default:
-        throw new Error(`Unsupported architecture on Windows: ${arch}`);
-    }
-    break;
-  case "darwin":
-    switch (arch) {
-      case "x64":
-        localFileExisted = existsSync(join(__dirname, "uuid.darwin-x64.node"));
-        try {
-          if (localFileExisted) {
-            nativeBinding = require("./uuid.darwin-x64.node");
-          } else {
-            nativeBinding = require("@napi-rs/uuid-darwin-x64");
-          }
-        } catch (e) {
-          loadError = e;
-        }
-        break;
-      case "arm64":
-        localFileExisted = existsSync(
-          join(__dirname, "uuid.darwin-arm64.node")
-        );
-        try {
-          if (localFileExisted) {
-            nativeBinding = require("./uuid.darwin-arm64.node");
-          } else {
-            nativeBinding = require("@napi-rs/uuid-darwin-arm64");
-          }
-        } catch (e) {
-          loadError = e;
-        }
-        break;
-      default:
-        throw new Error(`Unsupported architecture on macOS: ${arch}`);
-    }
-    break;
-  case "freebsd":
-    if (arch !== "x64") {
-      throw new Error(`Unsupported architecture on FreeBSD: ${arch}`);
-    }
-    localFileExisted = existsSync(join(__dirname, "uuid.freebsd-x64.node"));
-    try {
-      if (localFileExisted) {
-        nativeBinding = require("./uuid.freebsd-x64.node");
-      } else {
-        nativeBinding = require("@napi-rs/uuid-freebsd-x64");
-      }
-    } catch (e) {
-      loadError = e;
-    }
-    break;
-  case "linux":
-    switch (arch) {
-      case "x64":
-        isMusl = readFileSync("/usr/bin/ldd", "utf8").includes("musl");
-        if (isMusl) {
+      case 'x64':
+        if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, "uuid.linux-x64-musl.node")
-          );
+            join(__dirname, 'uuid.linux-x64-musl.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require("./uuid.linux-x64-musl.node");
+              nativeBinding = require('./uuid.linux-x64-musl.node')
             } else {
-              nativeBinding = require("@napi-rs/uuid-linux-x64-musl");
+              nativeBinding = require('@napi-rs/uuid-linux-x64-musl')
             }
           } catch (e) {
-            loadError = e;
+            loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, "uuid.linux-x64-gnu.node")
-          );
+            join(__dirname, 'uuid.linux-x64-gnu.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require("./uuid.linux-x64-gnu.node");
+              nativeBinding = require('./uuid.linux-x64-gnu.node')
             } else {
-              nativeBinding = require("@napi-rs/uuid-linux-x64-gnu");
+              nativeBinding = require('@napi-rs/uuid-linux-x64-gnu')
             }
           } catch (e) {
-            loadError = e;
+            loadError = e
           }
         }
-        break;
-      case "arm64":
-        isMusl = readFileSync("/usr/bin/ldd", "utf8").includes("musl");
-        if (isMusl) {
+        break
+      case 'arm64':
+        if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, "uuid.linux-arm64-musl.node")
-          );
+            join(__dirname, 'uuid.linux-arm64-musl.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require("./uuid.linux-arm64-musl.node");
+              nativeBinding = require('./uuid.linux-arm64-musl.node')
             } else {
-              nativeBinding = require("@napi-rs/uuid-linux-arm64-musl");
+              nativeBinding = require('@napi-rs/uuid-linux-arm64-musl')
             }
           } catch (e) {
-            loadError = e;
+            loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, "uuid.linux-arm64-gnu.node")
-          );
+            join(__dirname, 'uuid.linux-arm64-gnu.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require("./uuid.linux-arm64-gnu.node");
+              nativeBinding = require('./uuid.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require("@napi-rs/uuid-linux-arm64-gnu");
+              nativeBinding = require('@napi-rs/uuid-linux-arm64-gnu')
             }
           } catch (e) {
-            loadError = e;
+            loadError = e
           }
         }
-        break;
-      case "arm":
+        break
+      case 'arm':
         localFileExisted = existsSync(
-          join(__dirname, "uuid.linux-arm-gnueabihf.node")
-        );
+          join(__dirname, 'uuid.linux-arm-gnueabihf.node')
+        )
         try {
           if (localFileExisted) {
-            nativeBinding = require("./uuid.linux-arm-gnueabihf.node");
+            nativeBinding = require('./uuid.linux-arm-gnueabihf.node')
           } else {
-            nativeBinding = require("@napi-rs/uuid-linux-arm-gnueabihf");
+            nativeBinding = require('@napi-rs/uuid-linux-arm-gnueabihf')
           }
         } catch (e) {
-          loadError = e;
+          loadError = e
         }
-        break;
+        break
       default:
-        throw new Error(`Unsupported architecture on Linux: ${arch}`);
+        throw new Error(`Unsupported architecture on Linux: ${arch}`)
     }
-    break;
+    break
   default:
-    throw new Error(`Unsupported OS: ${platform}, architecture: ${arch}`);
+    throw new Error(`Unsupported OS: ${platform}, architecture: ${arch}`)
 }
 
 if (!nativeBinding) {
   if (loadError) {
-    throw loadError;
+    throw loadError
   }
-  throw new Error(`Failed to load native binding`);
+  throw new Error(`Failed to load native binding`)
 }
 
-const { v4 } = nativeBinding;
+const { v4 } = nativeBinding
 
-module.exports.v4 = v4;
+module.exports.v4 = v4

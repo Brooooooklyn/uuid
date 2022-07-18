bdc2b7bb2cddd9d8b0e5dcb0909340ec8d4ca756
LongYinan <lynweklm@gmail.com>
2022-07-18 10:24:38 +0800

    chore: upgrade dependencies (#41)

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@
 
 use napi_derive::napi;
 
+#[cfg(not(all(target_os = "linux", target_env = "musl", target_arch = "aarch64")))]
+#[global_allocator]
+static ALLOC: mimalloc_rust::GlobalMiMalloc = mimalloc_rust::GlobalMiMalloc;
+
 #[napi]
 pub fn v4() -> String {
   uuid::Uuid::new_v4().to_string()

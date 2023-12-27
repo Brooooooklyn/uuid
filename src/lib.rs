30ef7fd41e622e9cf062c66d12e2e95cae4ab2b0
LongYinan <lynweklm@gmail.com>
2023-12-27 18:59:34 +0800

    feat: upgrade all dependencies (#95)

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,8 @@
 
 use napi_derive::napi;
 
-#[cfg(not(all(target_os = "linux", target_env = "musl", target_arch = "aarch64")))]
 #[global_allocator]
-static ALLOC: mimalloc_rust::GlobalMiMalloc = mimalloc_rust::GlobalMiMalloc;
+static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[napi]
 #[inline(always)]
